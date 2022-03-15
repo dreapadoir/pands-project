b8a712be90789d3d0b5e6257440de818ac48dbc8
David Higgins <G00411302@gmit.ie>
2022-03-15 19:29:39 +0000

    add average sepal lengths for each variety

--- irisdataset.py
+++ irisdataset.py
@@ -10,3 +10,9 @@ irisData = data.frame
 setosaData = irisData.iloc[:50]
 versicolorData = irisData.iloc[50:100]
 virginicaData = irisData.iloc[100:150]
+
+avg = setosaData['sepal length (cm)'].sum(axis = 0)/len(setosaData['sepal length (cm)'])
+avg1 = versicolorData['sepal length (cm)'].sum(axis = 0)/len(versicolorData['sepal length (cm)'])
+avg2 = virginicaData['sepal length (cm)'].sum(axis = 0)/len(virginicaData['sepal length (cm)'])
+
+print('Average sepal lengths are:', round(avg, 3), round(avg1, 3), round(avg2, 3))
