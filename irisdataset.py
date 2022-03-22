9a3fe26c3f8484f81c8534d1aed6fd9ad2f2b422
David Higgins <G00411302@gmit.ie>
2022-03-22 17:53:12 +0000

    add summary statistics using pandas agg function and groupby

--- irisdataset.py
+++ irisdataset.py
@@ -2,6 +2,8 @@
 #Author: David Higgins
 
 from sklearn.datasets import load_iris
+from matplotlib import pyplot as plt
+import pandas as pd
 
 data = load_iris(as_frame=True)
 
@@ -11,8 +13,16 @@ setosaData = irisData.iloc[:50]
 versicolorData = irisData.iloc[50:100]
 virginicaData = irisData.iloc[100:150]
 
-avg = setosaData['sepal length (cm)'].sum(axis = 0)/len(setosaData['sepal length (cm)'])
-avg1 = versicolorData['sepal length (cm)'].sum(axis = 0)/len(versicolorData['sepal length (cm)'])
-avg2 = virginicaData['sepal length (cm)'].sum(axis = 0)/len(virginicaData['sepal length (cm)'])
+print("Target codes:\n0 =", data.target_names[0], "\n1 =", data.target_names[1], "\n2 =", data.target_names[2])
 
-print('Average sepal lengths are:', round(avg, 3), round(avg1, 3), round(avg2, 3))
+print(irisData.groupby('target').agg(
+    {
+        'sepal length (cm)': ["min", "max", "mean" ],
+        'sepal width (cm)': ["min", "max", "mean"],
+        'petal length (cm)': ["min", "max", "mean"],
+        'petal width (cm)': ["min", "max", "mean"]
+    }
+
+)
+)
+
