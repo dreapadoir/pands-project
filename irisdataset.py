9eb19fdc0d0173d47861283f384a03e4fca710f2
David Higgins <g00411302@gmit.ie>
2022-04-06 20:26:36 +0100

    add standard deviation calculations and confidence intervals

--- irisdataset.py
+++ irisdataset.py
@@ -4,6 +4,8 @@
 from sklearn.datasets import load_iris          #loads the modules necessary for this code
 from matplotlib import pyplot as plt
 import pandas as pd
+import numpy as np
+import scipy.stats
 
 data = load_iris(as_frame=True)                 #loads the iris dataset as a dataframe
 
@@ -30,3 +32,25 @@ sepLenBx = [                                #creates a list containing the sepal
 
 plt.boxplot(sepLenBx, labels = labels)      #plots boxplots of each set of data in sepLenBx and assigns the variety names as labels
 plt.show()
+
+print(irisData.groupby('target').agg(   #finds the standard deviation of the four attributes for each variety of iris
+    {
+        'sepal length (cm)': ["std"],
+        'sepal width (cm)': ["std"],
+        'petal length (cm)': ["std"],
+        'petal width (cm)': ["std"]
+    }
+)
+)
+
+def mean_confidence_interval(data, confidence=0.95):      #uses scipy and numpy to create a 95% confidence interval
+    a = 1.0 * data
+    n = len(a)
+    m, se = np.mean(a), scipy.stats.sem(a)
+    h = se * scipy.stats.t.ppf((1 + confidence) / 2., n-1)
+    return print('The 95% confidence interval is ({}, {})'.format(round(m-h, 3), round(m+h, 3)))
+
+intervals = [range(0, 50), range(50, 100), range(100,150)]
+
+for interval in intervals:
+        mean_confidence_interval(irisData['petal length (cm)'][interval])
